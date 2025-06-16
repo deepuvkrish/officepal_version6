ea3e6879f64cf6ae0f5fbdd8e405cb1133dc2807
Deepak K V <deepudeepz383@gmail.com>
2025-06-16 13:54:14 +0530

    vercel json's type error solution -7

---
+++ next.config.ts
@@ -0,0 +1,42 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    serverActions: {}, // ✅ Correct format for Next.js 15+
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
+    ],
+  },
+}
+
+
+export default nextConfig
+
+// // next.config.js
+// /** @type {import('next').NextConfig} */
+// module.exports = {
+//   reactStrictMode: true,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "lh3.googleusercontent.com",
+//       },
+//     ],
+//   },
+//   experimental: {
+//     serverActions: {
+//       // optional: increase body size if needed (only valid shape)
+//       bodySizeLimit: "2mb"
+//     },
+//   },
+// }
+
+
